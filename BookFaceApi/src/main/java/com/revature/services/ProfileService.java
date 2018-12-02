ec0087c94fc2426a6c7d55c5a980867b31867b55
James <jdula1990@gmail.com>
2018-12-02 15:17:34 -0600

    profile functionality?

--- BookFaceApi/src/main/java/com/revature/services/ProfileService.java
+++ BookFaceApi/src/main/java/com/revature/services/ProfileService.java
@@ -5,7 +5,6 @@ import java.sql.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.revature.models.Profile;
 import com.revature.repos.ProfileRepo;
